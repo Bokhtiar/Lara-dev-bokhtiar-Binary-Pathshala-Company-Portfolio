60fda7f51c200ba7221bd01759acd2da5cd3a84b
Bokhtiar Tushar <bokhtiartoshar1@gmail.com>
2022-06-09 00:38:49 +0600

    team store and list done

--- app/Http/Controllers/Admin/TeamController.php
+++ app/Http/Controllers/Admin/TeamController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Team;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TeamController extends Controller
 {
@@ -14,7 +16,12 @@ class TeamController extends Controller
      */
     public function index()
     {
-        //
+        try {
+            $teams = Team::latest()->get(['team_id','name', 'designation']);
+            return view('admin.modules.team.index', compact('teams'));
+        } catch (\Throwable $th) {
+            throw $th;
+        }
     }
 
     /**
@@ -35,7 +42,32 @@ class TeamController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validated = Team::query()->Validation($request->all());
+        if($validated){
+            try{
+                DB::beginTransaction();
+                $image = Team::query()->Image($request);
+                $team = Team::create([
+                    'name' => $request->name,
+                    'designation' => $request->designation,
+                    'image' => json_encode($image),
+
+                    'fb' => $request->fb,
+                    'instagram' => $request->instagram,
+                    'twitter' => $request->twitter,
+                    'linkdin' => $request->linkdin,
+                ]);
+
+                if (!empty($team)) {
+                    DB::commit();
+                    return redirect()->route('admin.team.index')->with('success','Team Created successfully!');
+                }
+                throw new \Exception('Invalid About Information');
+            }catch(\Exception $ex){
+                return back()->withError($ex->getMessage());
+                DB::rollBack();
+            }
+        }
     }
 
     /**
--- resources/views/admin/modules/team/index.blade.php
+++ resources/views/admin/modules/team/index.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.admin.app')
-@section('title', 'List Of Portfolio')
+@section('title', 'List Of Team')
 @section('css')
 @endsection
 
 @section('admin_content')
 
     <div class="pagetitle">
-        <h1>Portfolio Tables</h1>
+        <h1>team Tables</h1>
         <nav>
             <ol class="breadcrumb">
                 <li class="breadcrumb-item"><a href="index.html">Home</a></li>
                 <li class="breadcrumb-item">Tables</li>
-                <li class="breadcrumb-item active">Portfolio Table</li>
+                <li class="breadcrumb-item active">Team Table</li>
             </ol>
         </nav>
     </div><!-- End Page Title -->
@@ -19,7 +19,6 @@
     <section class="section">
         <div class="row">
             <div class="col-lg-12">
-
                 <div class="card">
                     <div class="card-header">
                         <x-notification></x-notification>
@@ -28,7 +27,7 @@
                         @endif
                     </div>
                     <div class="card-body">
-                        <h5 class="card-title">Portfolio Table</h5>
+                        <h5 class="card-title">Team Table</h5>
 
                         <!-- Table with stripped rows -->
                         <table class="table datatable">
@@ -36,35 +35,35 @@
                                 <tr>
                                     <th scope="col">SL</th>
                                     <th scope="col">Name</th>
-                                    <th scope="col">Service Name</th>
+                                    <th scope="col">Designation</th>
                                     <th scope="col">Status</th>
                                     <th scope="col">Action</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                @forelse ($portfolios as $item)
+                                @forelse ($teams as $item)
                                     <tr>
                                         <th scope="row">{{ $loop->index + 1 }}</th>
 
-                                        <td>{{ $item->title }}</td>
+                                        <td>{{ $item->name }}</td>
 
-                                        <td>{{ $item->service ? $item->service->name : "Data not Fount" }}</td>
+                                        <td>{{ $item->designation }}</td>
 
                                         <td>
                                             @if ($item->status == 1)
-                                                <a class="btn btn-sm btn-success" href="@route('admin.portfolio.status', $item->portfolio_id)"><i
+                                                <a class="btn btn-sm btn-success" href="@route('admin.team.status', $item->team_id)"><i
                                                         class="bi bi-check-circle"></i></a>
                                             @else
-                                                <a class="btn btn-warning btn-sm" href="@route('admin.portfolio.status', $item->portfolio_id)"><i
+                                                <a class="btn btn-warning btn-sm" href="@route('admin.team.status', $item->team_id)"><i
                                                         class="bi bi-exclamation-triangle"></i></a>
                                             @endif
                                         </td>
                                         <td class="form-inline">
-                                            <a class="btn btn-sm btn-info text-light" href="@route('admin.portfolio.show', $item->portfolio_id)"> <i
+                                            <a class="btn btn-sm btn-info text-light" href="@route('admin.team.show', $item->team_id)"> <i
                                                     class="ri-eye-fill"></i></a>
-                                            <a class="btn btn-sm btn-primary" href="@route('admin.portfolio.edit', $item->portfolio_id)"> <i
+                                            <a class="btn btn-sm btn-primary" href="@route('admin.team.edit', $item->team_id)"> <i
                                                     class="ri-edit-box-fill"></i></a>
-                                            <form method="POST" action="@route('admin.portfolio.destroy',$item->portfolio_id)" class="mt-1">
+                                            <form method="POST" action="@route('admin.team.destroy',$item->team_id)" class="mt-1">
                                                 @csrf
                                                 @method('Delete')
                                                 <button class="btn btn-sm btn-danger" type="submit"> <i
@@ -75,7 +74,7 @@
                                         </td>
                                     </tr>
                                 @empty
-                                    <h2 class="bg-danger text-light text-center">Portfolio Is empty</h2>
+                                    <h2 class="bg-danger text-light text-center">Team Is empty</h2>
                                 @endforelse
                             </tbody>
                         </table>
